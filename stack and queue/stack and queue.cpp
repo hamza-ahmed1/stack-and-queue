f9a3c92976205cdd7fe5665cb26047db9045b6ff
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-10-20 07:00:24 -0700

    Code Added

--- stack and queue/stack and queue.cpp
+++ stack and queue/stack and queue.cpp
@@ -4,52 +4,6 @@
 #include <iostream>
 using namespace std;
 
-class Stack
-{
-	int top = -1;
-	int arr[10] = {0};
-
-public:
-	void push(int val)
-	{
-		if (top >= 10)
-		{
-			cout << "Out of bound ,\n";
-			return;
-		}
-		arr[top + 1] = val;
-		top++;
-	}
-
-	void pop()
-	{
-		if (top == -1)
-		{
-			cout << "Stack is empty,\n";
-		}
-		arr[top] = 0;
-		top--;
-	}
-
-	void is_empty()
-	{
-		if (top == -1)
-		{
-			cout << "Stack is empty,\n";
-		}
-	}
-	void Top()
-	{
-		if (top == -1)
-		{
-			cout << "Stack is empty,\n";
-		}
-		else {
-			cout << arr[top] << endl;
-		}
-	}
-};
-
 class Node
 {
 
@@ -61,6 +15,62 @@ public:
 	{
 		this->data = data;
 		nextaddress = NULL;
+	}
+};
+
+class Stack
+{
+public:
+	Node* top = NULL;
+	Stack()
+	{
+
+	}
+	void Push(int data)
+	{
+		Node* newnode = new Node(data);
+		if (!top)
+		{
+			top = newnode;//updating top
+			return;
+		}
+		newnode->nextaddress = top;
+		top = newnode;
+	}
+
+	void Pop()
+	{
+		Node* temp = top;
+		if (top->nextaddress != NULL)
+		{
+			top = top->nextaddress;
+		}
+
+		temp = NULL;
+		delete temp;
+	}
+
+	void Peek()
+	{
+		if (top)
+		{
+			cout << top->data << endl;
+		}
+		else
+		{
+			cout << "Stack is empty" << endl;
+		}
+	}
+	void Isempty()
+	{
+		if (!top)
+		{
+			cout << "Stack is empty" << endl;
+		}
+		else
+		{
+			cout << "Stack is not empty" << endl;
+		}
 	}
 };
 class Queue
@@ -116,11 +126,6 @@ public:
 
 int main()
 {
-	Stack s;
-	s.is_empty();
-	s.push(6);
-	s.Top();
-	s.pop();
-	s.pop();
+
 }
 
