b2648dbaa1fd69ec17b6f82b0365dd86a17e1c56
Hamza Khan <106343759+hamza-ahmed1@users.noreply.github.com>
2023-10-20 06:58:55 -0700

    Added Code

--- stack and queue/stack and queue.cpp
+++ stack and queue/stack and queue.cpp
@@ -50,10 +50,68 @@ public:
 	}
 };
 
-class queue
-
+class Node
 {
 
+public:
+	int data;
+	Node* nextaddress;
+
+	Node(int data)
+	{
+		this->data = data;
+		nextaddress = NULL;
+	}
+};
+class Queue
+{
+public:
+	Node* front = NULL;
+	Node* rear = NULL;
+
+	Queue() {}
+
+	void Enqueue(int data)
+	{
+		Node* newnode = new  Node(data);
+		if (!rear)
+		{
+			rear = newnode;
+			front = newnode;
+		}
+		rear->nextaddress = newnode;
+		rear = newnode;
+	}
+
+	void dequeue()
+	{
+		Node* temp = front;
+		if (rear)
+		{
+			front = front->nextaddress;
+		}
+		temp = NULL;
+		delete temp;
+	}
+
+	void Peek()
+	{
+		if (rear)
+		{
+			cout << front->data << endl;
+		}
+	}
+	void Is_empty()
+	{
+		if (!front)
+		{
+			cout << "Queue is Empty" << endl;
+		}
+		else
+		{
+			cout << "Queue is not empty" << endl;
+		}
+	}
 };
 
 int main()
